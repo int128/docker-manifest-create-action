710d0c034e80714452d536957696db2c926fd39d
Hidetake Iwata <int128@gmail.com>
2023-03-18 15:58:27 +0900

    Add test of buildx (#70)

--- tests/run.test.ts
+++ tests/run.test.ts
@@ -3,7 +3,86 @@ import { getSourceManifests, run } from '../src/run'
 
 jest.mock('@actions/exec')
 
-describe('run', () => {
+const arrayEquals = <E>(a: E[], b: E[]) => a.length === b.length && a.every((_, i) => a[i] === b[i])
+
+describe('using buildx', () => {
+  beforeEach(() => jest.mocked(exec).exec.mockResolvedValue(0))
+  test('push a branch', async () => {
+    await run({
+      tags: ['ghcr.io/int128/docker-manifest-create-action:main'],
+      suffixes: ['-amd64', '-arm64'],
+      useBuildx: 'auto',
+    })
+    expect(exec.exec).toHaveBeenCalledWith('docker', [
+      'buildx',
+      'imagetools',
+      'create',
+      '-t',
+      'ghcr.io/int128/docker-manifest-create-action:main',
+      'ghcr.io/int128/docker-manifest-create-action:main-amd64',
+      'ghcr.io/int128/docker-manifest-create-action:main-arm64',
+    ])
+    expect(exec.exec).toHaveBeenCalledWith('docker', [
+      'buildx',
+      'imagetools',
+      'inspect',
+      'ghcr.io/int128/docker-manifest-create-action:main',
+    ])
+  })
+
+  test('push a tag with latest', async () => {
+    await run({
+      tags: [
+        'ghcr.io/int128/docker-manifest-create-action:v1.0.0',
+        'ghcr.io/int128/docker-manifest-create-action:latest',
+      ],
+      suffixes: ['-amd64'],
+      useBuildx: 'auto',
+    })
+
+    // non-latest tag
+    expect(exec.exec).toHaveBeenCalledWith('docker', [
+      'buildx',
+      'imagetools',
+      'create',
+      '-t',
+      'ghcr.io/int128/docker-manifest-create-action:v1.0.0',
+      'ghcr.io/int128/docker-manifest-create-action:v1.0.0-amd64',
+    ])
+    expect(exec.exec).toHaveBeenCalledWith('docker', [
+      'buildx',
+      'imagetools',
+      'inspect',
+      'ghcr.io/int128/docker-manifest-create-action:v1.0.0',
+    ])
+
+    // latest tag
+    expect(exec.exec).toHaveBeenCalledWith('docker', [
+      'buildx',
+      'imagetools',
+      'create',
+      '-t',
+      'ghcr.io/int128/docker-manifest-create-action:latest',
+      'ghcr.io/int128/docker-manifest-create-action:v1.0.0-amd64',
+    ])
+    expect(exec.exec).toHaveBeenCalledWith('docker', [
+      'buildx',
+      'imagetools',
+      'inspect',
+      'ghcr.io/int128/docker-manifest-create-action:latest',
+    ])
+  })
+})
+
+describe('using docker', () => {
+  beforeEach(() =>
+    jest.mocked(exec).exec.mockImplementation((_, args) => {
+      if (args && arrayEquals(args, ['buildx', 'version'])) {
+        return new Promise((resolve) => resolve(1)) // buildx is not available
+      }
+      return new Promise((resolve) => resolve(0))
+    })
+  )
   test('push a branch', async () => {
     await run({
       tags: ['ghcr.io/int128/docker-manifest-create-action:main'],
